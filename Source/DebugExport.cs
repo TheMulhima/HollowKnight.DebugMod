e9532d22a9095ec4c37a8c2ec914eb662de2ced1
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2021-11-29 22:57:57 +0400

    Add additional function to DebugExport

--- Source/DebugExport.cs
+++ Source/DebugExport.cs
@@ -12,5 +12,8 @@ namespace DebugMod
     {
         public static void AddActionToKeyBindList(Action method, string name, string category) 
             => DebugMod.AddActionToKeyBindList(method, name, category);
+
+        public static void LogToConsole(string msg)
+            => Console.AddLine(msg);
     }
 }
