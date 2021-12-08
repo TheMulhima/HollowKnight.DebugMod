5bcf3b815ef5e4b594a080122592bd418f0ad4e1
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-12-08 23:06:44 +0000

    Make DebugImport in example actually work

--- Example of Adding into Debug/AltDebugAddition.cs
+++ Example of Adding into Debug/AltDebugAddition.cs
@@ -27,19 +27,45 @@ namespace DebugAddition
 		    	DebugMod.LogToConsole("This goes to the debug console");
 		    }
     }
-    
+
     // We need to add this class - if debug is installed, MonoMod will automatically fill in the actions
-    // when we call   typeof(DebugMod).ModInterop();
-    [ModImportName("DebugMod")]
     internal static class DebugMod
     {
-        public static Action<Action, string, string> AddActionToKeyBindList = null;
-        public static Action<string> LogToConsole = null;
-        public static Func<bool, string> GetStringForBool = null;
-        public static Action<string, bool> CreateCustomInfoPanel = null;
-        public static Action<string, float, float, float, string, Func<string>> AddInfoToPanel = null;
-        public static Action<string, float> CreateSimpleInfoPanel = null;
-        public static Action<string, string, Func<string>> AddInfoToSimplePanel = null;
+        [ModImportName("DebugMod")]
+        private static class DebugImport
+        {
+            public static Action<Action, string, string> AddActionToKeyBindList = null;
+            public static Action<string> LogToConsole = null;
+            public static Func<bool, string> GetStringForBool = null;
+            public static Action<string, bool> CreateCustomInfoPanel = null;
+            public static Action<string, float, float, float, string, Func<string>> AddInfoToPanel = null;
+            public static Action<string, float> CreateSimpleInfoPanel = null;
+            public static Action<string, string, Func<string>> AddInfoToSimplePanel = null;
+        }
+        static DebugMod()
+        {
+            typeof(DebugImport).ModInterop();
+        }
 
-    } 
+        public static void AddActionToKeyBindList(Action method, string name, string category)
+            => DebugImport.AddActionToKeyBindList?.Invoke(method, name, category);
+
+        public static void LogToConsole(string msg)
+            => DebugImport.LogToConsole?.Invoke(msg);
+
+        public static string GetStringForBool(bool b) 
+            => DebugImport.GetStringForBool?.Invoke(b) ?? "";
+
+        public static void CreateCustomInfoPanel(string Name, bool ShowSprite)
+            => DebugImport.CreateCustomInfoPanel?.Invoke(Name, ShowSprite);
+
+        public static void AddInfoToPanel(string Name, float xLabel, float xInfo, float y, string label, Func<string> textFunc)
+            => DebugImport.AddInfoToPanel?.Invoke(Name, xLabel, xInfo, y, label, textFunc);
+
+        public static void CreateSimpleInfoPanel(string Name, float sep)
+            => DebugImport.CreateSimpleInfoPanel?.Invoke(Name, sep);
+
+        public static void AddInfoToSimplePanel(string Name, string label, Func<string> textFunc)
+            => DebugImport.AddInfoToSimplePanel?.Invoke(Name, label, textFunc);
+    }
 }
